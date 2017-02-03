bf06f4c1d82e9848ccf2ab861fcb84a27e3ceaa6
Kiwayne-Campbell <kiwayne.campbell@gmail.com>
2017-02-03 16:15:44 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+ // This script and many more are available free online at
+ // The JavaScript Source!! http://www.javascriptsource.com
+
+console.log('hello');
+
+// Insert number of questionsf
+var numQues = 4;
+
+// Insert number of choices in each question
+var numChoi = 3;
+
+// Insert number of questions displayed in answer area
+var answers = new Array(4);
+
+// Insert answers to questions
+answers[0] = "Cascading Style Sheets";
+answers[1] = "Dynamic HTML";
+answers[2] = "Netscape";
+answers[3] = "Common Gateway Interface";
+
+// Do not change anything below here ...
+function getScore(form) {
+  let score = 0;
+  for (i=0; i<numQues; i++) {
+    const currElt = i*numChoi;
+    for (j=0; j<numChoi; j++) {
+      const currSelection = form.elements[currElt + j];
+      if (currSelection.checked) {
+        if (currSelection.value == answers[i]) {
+          score++;
+          break;
+        }
+      }
+    }
+  }
+  score = Math.round(score);
+  form.percentage.value = score;
+  let correctAnswers = "";
+  for (i=1; i<=numQues; i++) {
+    correctAnswers += i + ". " + answers[i-1] + "\r\n";
+  }
+  form.solutions.value = correctAnswers;
+}
